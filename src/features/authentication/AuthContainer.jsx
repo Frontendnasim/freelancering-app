c519668ac1d2cfb8015d7623295ed9b7dd15758d
WinterBreeze97 <sophia.stallings97@gmail.com>
2023-12-23 19:29:36 +0330

    Create sendOtp, checkOtp, completeProfile

---
+++ src/features/authentication/AuthContainer.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import SendOTPForm from "./SendOTPForm";
+import CheckOTPForm from "./CheckOTPForm";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { getOtp } from "../../services/authService";
+
+function AuthContainer() {
+  const {
+    isPending: isSendingOtp,
+    mutateAsync,
+    data: otpResponse,
+  } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const sendOtpHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await mutateAsync({ phoneNumber });
+      setStep(2);
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+  const [step, setStep] = useState(1);
+  const [phoneNumber, setPhoneNumber] = useState("09109879213");
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return (
+          <SendOTPForm
+            isSendingOtp={isSendingOtp}
+            onSubmit={sendOtpHandler}
+            setStep={setStep}
+            phoneNumber={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        );
+      case 2:
+        return (
+          <CheckOTPForm
+            onResendOtp={sendOtpHandler}
+            phoneNumber={phoneNumber}
+            onBack={() => setStep((s) => s - 1)}
+            otpResponse={otpResponse}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+  return <div className="w-full sm:max-w-sm">{renderStep()}</div>;
+}
+
+export default AuthContainer;
--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,38 +1,25 @@
-import { useState } from "react";
 import TextField from "../../ui/TextField";
-import { useMutation } from "@tanstack/react-query";
-import { getOtp } from "../../services/authService";
-import toast from "react-hot-toast";
+import Loading from "../../ui/Loading";
 
-function SendOTPForm() {
-  const [phoneNumber, setPhoneNumber] = useState("");
-
-  const { isPending, error, data, mutateAsync } = useMutation({
-    mutationFn: getOtp,
-  });
-
-  const sendOtpHandler = async (e) => {
-    e.preventDefault();
-    try {
-      const data = await mutateAsync({ phoneNumber });
-      toast.success(data.message);
-    } catch (error) {
-      toast.error(error?.response?.data?.message);
-    }
-  };
-
+function SendOTPForm({ onSubmit, isSendingOtp, phoneNumber, onChange }) {
   return (
     <div>
-      <form className="space-y-8" onSubmit={sendOtpHandler}>
+      <form className="space-y-8" onSubmit={onsubmit}>
         <TextField
           label="شماره موبایل"
           name="phoneNumber"
           value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
+          onChange={onChange}
         />
-        <button type="submit" className="btn btn--primary w-full">
-          ارسال کد تایید
-        </button>
+        <div>
+          {isSendingOtp ? (
+            <Loading />
+          ) : (
+            <button type="submit" className="btn btn--primary w-full">
+              ارسال کد تایید
+            </button>
+          )}
+        </div>
       </form>
     </div>
   );
